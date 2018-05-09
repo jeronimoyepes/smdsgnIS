2ee6724642fc1e84710dec6e7fb12d9f1ab38e1a
Jerónimo Yepes <jeronimo_y_g@hotmail.com>
2018-05-09 18:40:27 -0500

    cambiar la posición de el JS en los documentos. Agregado el JS para formularios

--- pre-js/interaction.js
+++ pre-js/interaction.js
@@ -1,5 +1,3 @@
-$(document).ready(function () {
-    $(".notification").click(function () {
+$(".notification").click(function () {
         $(".noti-feed").slideToggle(300);
-    })
 });
--- pre-js/styling.js
+++ pre-js/styling.js
@@ -1,39 +1,35 @@
-$(document).ready(function($) {
 
-    var mediaQueryTablet = 991;
+var mediaQueryTablet = 991;
 
-    // Hacer que el contenedor del contenido principal sea igual al ancho de la ventana menos el ancho de los contenedores de los lados
-    function resizeMainCont (offset) {
-        $(".cont-main").width(function () {
-            return $(".wrapper").width() - ($(".cont-left").width() + $(".cont-right").width()) - offset;
-        });
-    } resizeMainCont (0);
-
-    function changeContRightPosition () {
-        if ($(window).width() <= mediaQueryTablet) {
-            $(".cont-right").insertBefore($(".cont-main"));
-        }else {
-            $(".cont-right").insertAfter($(".cont-main"));
-        }
-    } changeContRightPosition ();
-    
-    // registrar el cambio de tamaño de la pantalla
-    $(window).resize(function () {
-        if ($(window).width() >= mediaQueryTablet) {
-            resizeMainCont (0);
-        }else {
-            $(".cont-main").width("initial");
-        };
-        changeContRightPosition();
+// Hacer que el contenedor del contenido principal sea igual al ancho de la ventana menos el ancho de los contenedores de los lados
+function resizeMainCont (offset) {
+    $(".cont-main").width(function () {
+        return $(".wrapper").width() - ($(".cont-left").width() + $(".cont-right").width()) - offset;
     });
+} resizeMainCont (0);
 
-    setInterval(function () {
-        if ($(window).width() > mediaQueryTablet) { 
-            resizeMainCont (0);
-        } else {
-            $(".cont-main").width("initial");
-        }
-    }, 100);
+function changeContRightPosition () {
+    if ($(window).width() <= mediaQueryTablet) {
+        $(".cont-right").insertBefore($(".cont-main"));
+    }else {
+        $(".cont-right").insertAfter($(".cont-main"));
+    }
+} changeContRightPosition ();
 
-    
+// registrar el cambio de tamaño de la pantalla
+$(window).resize(function () {
+    if ($(window).width() >= mediaQueryTablet) {
+        resizeMainCont (0);
+    }else {
+        $(".cont-main").width("initial");
+    };
+    changeContRightPosition();
 });
+
+setInterval(function () {
+    if ($(window).width() > mediaQueryTablet) { 
+        resizeMainCont (0);
+    } else {
+        $(".cont-main").width("initial");
+    }
+}, 100);
